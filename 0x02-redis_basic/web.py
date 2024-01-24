e120b0f47e555a0e08c91d1d4492c2b1dff0fa4a
mouezelaifa <mouezelaifa@gmail.com>
2024-01-24 11:45:40 -0800

    0x02. Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,45 +1,22 @@
 #!/usr/bin/env python3
 """
-Module to create a web cache using Redis.
+create a web cach
 """
-
 import redis
 import requests
-
-# Create a Redis client
 rc = redis.Redis()
-
-# Initialize count
 count = 0
 
 
 def get_page(url: str) -> str:
-    """
-    Fetch a webpage and cache its content using Redis.
-
-    Parameters:
-    url (str): The URL of the webpage to fetch.
-
-    Returns:
-    str: The content of the webpage.
-    """
-
-    # Cache the count associated with the URL
+    """ get a page and cach value"""
     rc.set(f"cached:{url}", count)
-
-    # Send a GET request to the URL
     resp = requests.get(url)
-
-    # Increment the count associated with the URL
     rc.incr(f"count:{url}")
-
-    # Set an expiration time on the cached count
     rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
-
-    # Return the text content of the webpage
     return resp.text
 
 
 if __name__ == "__main__":
-    # Fetch and cache a webpage
-    get_page("http://slowwly.robertomurray.co.uk")
+    get_page('http://slowwly.robertomurray.co.uk')
+
