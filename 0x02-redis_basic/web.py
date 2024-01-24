65c3750abe8bc10237b67d1cce08eb6deb80f8eb
mouezelaifa <mouezelaifa@gmail.com>
2024-01-24 12:01:27 -0800

    0x02. Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python3
-"""
-This module defines the function `get_page` as well as a decorator for keeping
-track of calls to `get_page` with a particular url
-"""
+""" Model for the get_page function """
 import redis
-import requests
-from functools import wraps
-from typing import Callable
+from requests import get
 
 
-def access_count(get_page: Callable[[str], str]) -> Callable[[str], str]:
-    """Track calls to `get_page` with a particular url """
-    @wraps(get_page)
-    def wrapper(url: str) -> str:
-        key = f"count:{url}"
-        store = redis.Redis()
-        store.incr(key)
-        store.expire(key, 10)
-        return get_page(url)
-    return wrapper
-
-
-@access_count
 def get_page(url: str) -> str:
-    """Obtain and return the HTML content of a particular URL, `url` """
-    return requests.get(url).text
+    """obtain the HTML content of a particular URL and returns it."""
+    r = redis.Redis()
+    content = r.get(url)
+    name = f"count:{url}"
+    r.incr(name)
+    if content:
+        return content.decode('utf-8')
+    content = get(url)
+    r.setex(url, 10, content.text)
+    r.set(f'count:{url}', 0)
+    return content.text
